4d1e90f2dfb3d2a24c53c65243b487f3a99234d5
SadmanYeamin <Sadman.Aiub17@gmail.com>
2022-01-07 14:05:23 +0600

    third commit

--- testingCRUDSearch/Controllers/BookController.cs
+++ testingCRUDSearch/Controllers/BookController.cs
@@ -27,5 +27,34 @@ namespace testingCRUDSearch.Controllers
             bookModel.Insert(book);
             return RedirectToAction("Index");
         }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            return View(bookModel.GetBook(id));
+        }
+        [HttpPost]
+        public ActionResult Edit(Book book)
+        {
+            bookModel.Update(book);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            return View(bookModel.Delete(id));
+        }
+
+        [HttpPost,ActionName("Delete")]
+        
+        public ActionResult ConfirmDelete(int id)
+        {
+            
+            bookModel.Delete(id);
+            return RedirectToAction("Index");
+        }
+
+
     }
 }
